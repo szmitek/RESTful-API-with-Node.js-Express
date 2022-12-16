4104860948662a9a84454c2cc063435c63151d74
Kamil S <61392709+szmitek@users.noreply.github.com>
2022-12-16 22:47:05 +0100

    :construction:  Added homeController for GET http://localhost:8080/

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 const app = require('express')();
+const homeController = require('../controllers/homeController');
 
 
+app.route('/').get(homeController.homePage)
 
 const PORT = process.env.PORT || 8080
 app.listen(
