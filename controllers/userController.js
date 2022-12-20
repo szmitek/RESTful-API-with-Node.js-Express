d37be065024cddd1d2aaa3562aba02c5b4609f6d
Kamil S <61392709+szmitek@users.noreply.github.com>
2022-12-20 18:45:17 +0100

    :wastebasket:  Clean up userController, added 404 status if request don't exist. Also added uuidv4 for generating unique IDs

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,40 +1,73 @@
+const uuidv4 = require('uuid/v4');  // For generating unique IDs
+
+// An array to store our users and their posts
 const users = [
     {
-        id: 1,
-        userName: 'kamil',
-    }
-]
+        id: '1',
+        name: 'Me',
+        posts: [
+            { id: '1', title: 'Post 1', description: 'Description 1' },
+            { id: '2', title: 'Post 2', description: 'Description 2' },
+        ],
+    },
+    {
+        id: '2',
+        name: 'Test',
+        posts: [
+            { id: '3', title: 'Post 3', description: 'Description 3' },
+            { id: '4', title: 'Post 4', description: 'Description 4' },
+        ],
+    },
+    {
+        id: '3',
+        name: 'Bob',
+        posts: [
+            { id: '5', title: 'Post 5', description: 'Description 5' },
+            { id: '6', title: 'Post 6', description: 'Description 6' },
+        ],
+    },
+];
 
 exports.createUser = (req, res) => {
-        const user = {
-            id: users.length + 1,
-            userName: req.body.userName,
-        }
-        users.push(user);
-        res.send(user);
-        res.status(201).json(users);
-}
+    const user = {
+        id: uuidv4(),  // Generate a unique ID
+        name: req.body.name,
+        posts: [],
+    };
+    users.push(user);
+    res.status(201).json(user);
+};
+
+exports.getUsers = (req, res) => {
+    res.status(200).json(users);
+};
 
 exports.getUser = (req, res) => {
-        res.send(users);
-        res.status(200).json(users);
-}
+    const user = users.find((x) => x.id === req.params.id);
+    if (user) {
+        res.status(200).json(user);
+    } else {
+        res.status(404).send('User not found');
+    }
+};
 
-exports.putUser = (req, res) => {
-    const user = users.find(
-        x => x.id === parseInt(req.params.id)
-    )
-    user.userName = req.body.userName
-    res.send(user);
-    res.status(200).json(user);
-}
+exports.updateUser = (req, res) => {
+    const user = users.find((x) => x.id === req.params.id);
+    if (user) {
+        user.name = req.body.name;
+        res.status(200).json(user);
+    } else {
+        res.status(404).send('User not found');
+    }
+};
 
 exports.deleteUser = (req, res) => {
-    const user = users.find(
-        x => x.id === parseInt(req.params.id)
-    )
-    const index = users.indexOf(user);
-    users.splice(index, 1);
-    res.send(user)
-    res.status(204).json(user);
-}
+    const user = users.find((x) => x.id === req.params.id);
+    if (user) {
+        const index = users.indexOf(user);
+        users.splice(index, 1);
+        res.status(204).send();
+    } else {
+        res.status(404).send('User not found');
+    }
+};
