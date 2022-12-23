016de9a3b7f096562e10664170e47e4fa556cc51
Kamil S <61392709+szmitek@users.noreply.github.com>
2022-12-23 12:13:28 +0100

    :construction:  get a list of posts for a user or specific post from user

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,7 +1,6 @@
-//const uuid = require('uuid');  // For generating unique IDs
-//const users = require('../data/users');
 const { db } = require('../config');
 
+// create a post for a user
 exports.createPost = (req, res) => {
     // get a reference to the user document
     const userRef = db.collection('users').doc(req.params.userId);
@@ -32,27 +31,56 @@ exports.createPost = (req, res) => {
         });
 };
 
+// get a list of posts for a user
 exports.getPosts = (req, res) => {
-    const user = users.find((x) => x.id === req.params.userId);
-    if (user) {
-        res.status(200).json(user.posts);
-    } else {
-        res.status(404).send('User not found');
-    }
+    // get a reference to the user document
+    const userRef = db.collection('users').doc(req.params.userId);
+
+    // get a reference to the "posts" subcollection of the user document
+    const postsRef = userRef.collection('posts');
+
+    // get all documents in the subcollection
+    postsRef.get()
+        .then((snapshot) => {
+            // build an array of posts from the query snapshot
+            const posts = snapshot.docs.map((doc) => {
+                return { id: doc.id, ...doc.data() };
+            });
+
+            // return the array of posts
+            res.send(posts);
+        })
+        .catch((error) => {
+            // handle any errors
+            console.error(error);
+            res.status(500).send('Something went wrong!');
+        });
 };
 
+// get a specific post for a user
 exports.getPost = (req, res) => {
-    const user = users.find((x) => x.id === req.params.userId);
-    if (user) {
-        const post = user.posts.find((x) => x.id === req.params.id);
-        if (post) {
-            res.status(200).json(post);
-        } else {
-            res.status(404).send('Post not found');
-        }
-    } else {
-        res.status(404).send('User not found');
-    }
+    // get a reference to the user document
+    const userRef = db.collection('users').doc(req.params.userId);
+
+    // get a reference to the specific post document
+    const postRef = userRef.collection('posts').doc(req.params.postId);
+
+    // get the data for the post
+    postRef.get()
+        .then((doc) => {
+            if (doc.exists) {
+                // return the post data
+                res.send(doc.data());
+            } else {
+                // return a 404 if the post was not found
+                res.status(404).send('Post not found');
+            }
+        })
+        .catch((error) => {
+            // handle any errors
+            console.error(error);
+            res.status(500).send('Something went wrong!');
+        });
 };
 
 exports.updatePost = (req, res) => {
--- routes/index.js
+++ routes/index.js
@@ -20,10 +20,10 @@ app.delete('/users/:id', userController.deleteUser);
 
 // Post routes
 app.get('/users/:userId/posts', postController.getPosts);
-app.get('/users/:userId/posts/:id', postController.getPost);
+app.get('/users/:userId/posts/:postId', postController.getPost);
 app.post('/users/:userId/posts', postController.createPost);
-app.put('/users/:userId/posts/:id', postController.updatePost);
-app.delete('/users/:userId/posts/:id', postController.deletePost);
+app.put('/users/:userId/posts/:postId', postController.updatePost);
+app.delete('/users/:userId/posts/:postId', postController.deletePost);
 
 const PORT = process.env.PORT || 8080
 app.listen(
